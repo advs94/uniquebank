7bbf6f6ee163dcc439489f2c0072181ccb369725
André V. Silva <advs.94@hotmail.com>
2019-05-11 18:27:19 +0100

    Life Detection 3.0

--- public/js/video.js
+++ public/js/video.js
@@ -1,34 +1,69 @@
 let mobilenet;
+let classifier;
 let video;
-let label = '';
+let label = 0;
+let trainButton;
+let addButton;
 
-function modelReady() {
-  console.log('Model is ready!!!');
-  mobilenet.predict(gotResults);
-}
-
-function gotResults(error, results) {
-  if(error) {
-    console.error(error);
+function whileTraining(loss) {
+  if(loss == null) {
+    console.log('Training Complete');
+    predictor.predict(gotResults);
   } else {
-    // console.log(results);
-    label = results[0].className;
-    mobilenet.predict(gotResults);
+    console.log(loss);
   }
 }
 
+function gotResults(error, result) {
+  if(error) {
+    console.error(error);
+  } else {
+    value = result;
+    predictor.predict(gotResults);
+  }
+}
+
+function modelReady() {
+  console.log('Model is ready!!!');
+}
+
+function videoReady() {
+  console.log('Video is ready!!!');
+}
+
 function setup() {
-  createCanvas(640, 550);
+  createCanvas(320, 270);
   video = createCapture(VIDEO);
   video.hide();
   background(0);
-  mobilenet = ml5.imageClassifier('MobileNet', video, modelReady);
+  mobilenet = ml5.featureExtractor('MobileNet', modelReady);
+  classifier = mobilenet.classification(video, videoReady);
+
+  happyButton = createButton('happy');
+  happyButton.mousePressed(function() {
+    classifier.addImage('happy');
+  });
+  
+  sadButton = createButton('sad');
+  sadButton.mousePressed(function() {
+    classifier.addImage('sad');
+  });
+ 
+  trainButton = createButton('train');
+  trainButton.mousePressed(function() {
+    classifier.train(whileTraining);
+  });
+  
+  saveButton = createButton('save');
+  saveButton.mousePressed(function() {
+    classifier.save();
+  });
 }
 
 function draw() {
     background(0);
-    image(video, 0, 0);
+    image(video, 0, 0, 320, 240);
     fill(255);
-    textSize(64);
-    text(label, 10, height - 20);
+    textSize(16);
+    text(label, 10, height - 10);
 }
