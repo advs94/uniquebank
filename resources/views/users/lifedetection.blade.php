9570d6d47b6d465a459b961d074afdd10454825a
André V. Silva <advs.94@hotmail.com>
2019-05-16 03:12:59 +0100

    Life Detection 9.0

--- resources/views/users/lifedetection.blade.php
+++ resources/views/users/lifedetection.blade.php
@@ -17,60 +17,43 @@
             </div>
         @endif
 
-        <form class="form-horizontal" method="post" action="/users/{{ $user->id }}">
-            @method('PATCH')
-            @csrf
-                
-            @foreach ($user->getFillableAttributes() as $fillableAttribute)
-                <?php $aux = ucwords(str_replace("_", " ", $fillableAttribute)); ?>
-                <div class="form-group">
-                    <label for={{ $fillableAttribute }} class="col-sm-2 control-label" style="max-width:none"><h5>{{ $aux }}</h5></label>
-                    @if (strtotime($user->$fillableAttribute))
-                        <?php
-                            $aux = explode('-', $user->$fillableAttribute);
-                            $temp = $aux[0];
-                            $aux[0] = $aux[2];
-                            $aux[2] = $temp;
-                            $aux = implode('/', $aux);
-                        ?>
-                        <div class="col-sm-10 has-feedback {{ $errors->has($fillableAttribute) ? 'has-error' : '' }}">
-                            <input type="text" id="{{ $fillableAttribute }}" name="{{ $fillableAttribute }}" class="form-control" value="{{ $aux }}"></textarea>
-                            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                            @if ($errors->has($fillableAttribute))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first($fillableAttribute) }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                    @else
-                        <div class="col-sm-10 has-feedback {{ $errors->has($fillableAttribute) ? 'has-error' : '' }}">
-                            <input type="text" id="{{ $fillableAttribute }}" name="{{ $fillableAttribute }}" class="form-control" value="{{ $user->$fillableAttribute }}"></textarea>
-                            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                            @if ($errors->has($fillableAttribute))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first($fillableAttribute) }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                    @endif
-                </div>
-            @endforeach
-            <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                    <button type="submit" class="btn btn-default" style="background-color: rgb(0, 100, 255);">Update</button>
-                </div>
-            </div>
-        </form>
-        <form method="post" action="/users/{{ $user->id }}">
-            @method('DELETE')
-            @csrf
+        <?php
+            try {
+                $lifedetection = response()->file('js\myKNNDataset.json');
+            } catch (Exception $exception) {
+                $lifedetection = null;
+            }
+        ?>
+
+        @if ($lifedetection)
+            <h3>You have Life Detection functionality activated</h3>
 
             <div class="form-group">
-                <div class="col-sm-offset-2">
+                <div class="col-sm-offset">
+                    <a href="/users/lifedetection/edit"></a>
+                    <button type="submit" class="btn btn-default" style="background-color: rgb(0, 100, 255); margin-left:6px; padding: 7px 15px">Update</button>                        
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-offset">
+                    <a href="/users/lifedetection/{{ $user->id }}"></a>
                     <button type="submit" class="btn btn-default" style="background-color: rgb(0, 100, 255); margin-left:6px; padding: 7px 15px">Delete</button>
                 </div>
             </div>
-        </form>
+        @else
+            <h3>You don't have Life Detection functionality activated</h3>
+
+            <form class="form-horizontal" method="get" action="/users/lifedetection/create">
+                @csrf
+
+                <div class="form-group">
+                    <div class="col-sm-offset-2">
+                        <button type="submit" class="btn btn-default" style="background-color: rgb(0, 100, 255); margin-left:6px; padding: 7px 15px">Activate</button>                        
+                    </div>
+                </div>
+            </form>
+        @endif
     </div>    
 @endsection
 
