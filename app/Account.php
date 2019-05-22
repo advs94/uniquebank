9d4a266b5c01f7878be235ff9675f3cc02be3473
André V. Silva <advs.94@hotmail.com>
2019-05-22 17:29:34 +0100

    Account Delete 1.0

--- app/Account.php
+++ app/Account.php
@@ -6,16 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
-    protected $guarded = [];
-
-    // /**
-    //  * The attributes that should be hidden for arrays.
-    //  *
-    //  * @var array
-    //  */
-    // protected $fillable = [
-    //     'balance', 'pin', 'nib', 'iban', 'user_id', 'type', 
-    // ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'balance', 'pin', 'nib', 'iban', 'user_id', 'type', 
+    ];
 
     /**
      * The attributes that should be cast to native types.
